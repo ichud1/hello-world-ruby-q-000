33191a3039c1a3e155a085405b94cbc026736b5d
israel lefkowitz <8458451@gmail.com>
2019-10-01 17:18:08 +0000

    Automatically backed up by Learn

--- spec/hello_world_spec.rb
+++ spec/hello_world_spec.rb
@@ -1,7 +1,4 @@
 describe "Hello World" do 
-  it 'has a file hello_world.rb' do
-    expect(File.exists?("hello_world.rb")).to be_truthy, "Did you create the file hello_world.rb? (If so, is it spelled exactly the same with the same capitalization?)"
-  end
 
   it 'puts "Hello World!"' do
     expect(1).to eq 1
